8f11e41b97058df2f70a034bbb1f696b2eeb8d0a
Alvaro Gomez <a.gomez191@usp.ceu.es>
2024-01-22 19:44:30 +0100

    Finished tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::fmt::Display;
+use std::fs::File;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
 use rand::Rng;
 use std::ops::Add;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+mod restaurant;
+use crate::restaurant::order_food;
 
 fn main() {
     println!("What is your name");
@@ -36,6 +46,8 @@ fn main() {
 
     let random_num = rand::thread_rng().gen_range(5..10);
     println!("random number: {}", random_num);
+
+    //If else
 
     let age = 8;
     if age >= 18 {
@@ -83,6 +95,7 @@ fn main() {
     println!("1st: {}", arr_1[0]);
     println!("Length: {}", arr_1.len());
 
+    //Loops
     let arr_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let mut loop_idx = 0;
     loop {
@@ -208,19 +221,236 @@ fn main() {
     println!("5.2 + 4.5 = {}", get_sum_gen(5.2, 4.5));
 
     //Ownerships
-    // Stack: fast, last in first out, fixed size
+    // Stack: fast, last in first out, fixed size, LIFO
     // Heap: slower, dynamic size, can grow in any direction
+
+    /*
+    Reglas:
+    Cada valor tiene una variable la cual es su dueño (owner)
+    Solo hay un dueño a la vez
+    Cuando el dueño está fuera del alcance el valor desaparece
+     */
 
     let str1 = String::from("World");
     //let str2 = str1;
-    let str2 = str1.clone();
     // Not possible to do: println!("Hello{}", str1);
+    let str2 = str1.clone();
     println!("Hello {}", str1);
 
     let str1 = String::from("Hello");
     let str2 = str1.clone();
     let str3 = print_return_str(str1);
     println!("{}", str3);
+
+    //Hash maps
+    let mut heroes = HashMap::new();
+    heroes.insert("Superman", "Clark kent");
+    heroes.insert("Batman", "Bruce Wayne");
+    heroes.insert("The flash", "Barry Allen");
+
+    for (k, v)  in heroes.iter() {
+        println!("{}{}", k, v);
+    }
+    if heroes.contains_key(&"Batman"){
+        let the_batman = heroes.get(&"Batman");
+        match the_batman {
+            Some(x) => println!("Batman is a hero"),
+            None => println!("Batman is not a hero"),
+        }
+    }
+
+    //Struct
+    struct Customer{
+        name: String,
+        address: String,
+        balance: f32,
+    }
+    let mut bob = Customer{
+        name: String::from("Bob Smith"),
+        address: String::from("555 Main St"),
+        balance: 234.50
+    };
+    bob.address = String::from("505 Main St");
+
+    struct Rectangle<T, U>{
+        length: T,
+        height: U,
+    }
+    let rec = Rectangle{length: 4, height: 10.5};
+    trait Shape{
+        fn new(length: f32, width: f32) -> Self;
+        fn area(&self) -> f32;
+    }
+
+    #[derive(Debug)]
+    struct NewRectangle {length: f32, width: f32}
+    #[derive(Debug)]
+    struct NewCircle {length: f32, width: f32}
+    impl Shape for NewRectangle {
+        fn new(length: f32, width: f32) -> NewRectangle {
+            return NewRectangle {length, width};
+        }
+        fn area(&self) -> f32{
+            return self.length * self.width;
+        }
+    }
+    impl Shape for NewCircle {
+        fn new(length: f32, width: f32) -> NewCircle {
+            return NewCircle {length, width};
+        }
+        fn area(&self) -> f32{
+            return (self.length / 2.0 ).powf(2.0) * PI;
+        }
+    }
+    let rec: NewRectangle = Shape::new(10.0, 10.0);
+    let circ: NewCircle = Shape::new(5.0, 4.0);
+    println!("Rec area {:?}", rec);
+    println!("Circ area {:?}", circ);
+
+    order_food();
+
+    //error handling
+    let path = "lines.txt";
+    let output = File::create(path);
+    let mut output = match output {
+        Ok(file) => file,
+        Err(error)=>{
+            panic!("problem creating file: {:?}", error);
+        }
+    };
+    write!(output, "Just some \n Randome words").expect("Failed to write to file");
+    let input = File::open(path).unwrap();
+    let buffered = BufReader::new(input);
+    for i in buffered.lines() {
+        println!("{}", i.unwrap());
+    }
+    let output2 = File::create("rand.txt");
+    let output2 = match output2 {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("rand.txt") {
+                Ok(fc) => fc,
+                Err(error) => panic!("Cant create file {:?}", error),
+            },
+            _other_error => panic!("problem opening file {:?}", error),
+        },
+    };
+
+    //Iterators
+    let mut arr_it = [1, 2, 3, 4];
+    for val in arr_it.iter() {
+        println!("{}", val);
+    }
+    let mut iter1 = arr_it.iter();
+    println!("1st: {:?}", iter1.next());
+
+    //Closure, funciton without name
+    let can_vote = | age: i32 | {
+        age >= 18
+    };
+    println!("Can vote: {:?}", can_vote(8));
+
+    let mut samp1 = 5;
+    let print_var = || println!("Samp1 = {}", samp1);
+    print_var();
+    samp1 = 10;
+    let mut change_var = || samp1 += 1;
+    change_var();
+    println!("samp1 = {}", samp1);
+    samp1 = 10;
+    println!("Samp1 = {}", samp1);
+
+    fn use_func<T>(a: i32, b: i32, func: T)-> i32 where T: Fn(i32, i32) -> i32{
+        func(a, b)
+    }
+    let sum = |a, b| a+b;
+    let prod = |a, b| a*b;
+    println!("5 + 4 = {}", use_func(5, 4, sum));
+    println!("5 + 4 = {}", use_func(5, 4, prod));
+
+    //box -> almacena datos en heap, gran cantidad datos
+    let b_int1 = Box::new(10);
+    println!("b_int1 = {}", b_int1);
+
+    struct TreeNode<T> {
+        pub left: Option<Box<TreeNode<T>>>,
+        pub right: Option<Box<TreeNode<T>>>,
+        pub key: T,
+    }
+    impl <T> TreeNode<T>{
+        pub fn new(key: T) -> Self{
+            TreeNode{left: None, right: None, key,}
+        }
+        pub fn left(mut self, node: TreeNode<T>)-> Self{
+            self.left = Some(Box::new(node));
+            self
+        }
+        pub fn right(mut self, node: TreeNode<T>)-> Self{
+            self.right = Some(Box::new(node));
+            self
+        }
+    }
+
+    let node1 = TreeNode::new(1).left(TreeNode::new(2)).right(TreeNode::new(3));
+
+    //Parallel programing
+    let thread1 = thread::spawn(||{
+        for i in 1..25 {
+            println!("Spawned thread: {}", i);
+            thread::sleep(Duration::from_millis(1))
+        }
+    });
+
+    for i in 1..20{
+        println!("Main thread: {}", i);
+        thread::sleep(Duration::from_millis(1))
+    }
+
+    thread1.join().unwrap();
+
+    pub struct Bank{
+        balance: f32
+    }
+    /*
+    fn withdraw(the_bank: &mut Bank, amt: f32){
+        the_bank.balance -= amt;
+    }
+    let mut bank = Bank{balance: 100.0};
+    withdraw(&mut bank, 5.00);
+    println!("Balance: {}", bank.balance);
+    fn customer(the_bank: &mut Bank){
+        withdraw(the_bank, 5.00);
+    }
+    thread::spawn(|| {
+        customer(&mut bank)
+    }).join().unwrap();
+    */
+    fn withdraw(the_bank: &Arc<Mutex<Bank>>, amt: f32){
+        let mut bank_ref = the_bank.lock().unwrap();
+        if bank_ref.balance < 5.00 {
+            println!("Current balance: {} Withdrawal a smaller amoutn", bank_ref.balance)
+        }
+        else { bank_ref.balance -= amt;
+            println!("Customer widthrew {} Current Balance {}", amt, bank_ref.balance)
+        }
+        fn customer(the_bank: Arc<Mutex<Bank>>){
+            withdraw(&the_bank, 5.00);
+        }
+        let bank: Arc<Mutex<Bank>> = Arc::new(Mutex::new(Bank{balance: 20.00}));
+        let handles = (0..10).map(|_| {
+            let bank_ref = bank.clone();
+            thread::spawn(|| {
+                customer(bank_ref)
+            })
+        });
+        for handle in handles  {
+            handle.join().unwrap();
+        }
+        println!("Total: {}", bank.lock().unwrap().balance )
+    }
+
+    //withdraw(&Arc::new(Mutex::new(Bank { balance: 20.0 })), 5.0);
+
 }
 
 fn say_hello(){
@@ -248,7 +478,7 @@ fn print_str(x: String){
 }
 
 fn print_return_str(x: String) -> String{
-    return x;
+   x
 }
 
 fn change_string(name: &mut String){
